636d0b3e57f70a5f900e9c59a763b4b8f56ba5bb
itsanindyak <iankoley04@gmail.com>
2025-05-11 01:33:36 +0530

    add user model with validation rules

---
+++ models/userModel.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	FirstName    string             `json:"first_name" validate:"required,min=3,max=20"`
+	LastName     string             `json:"last_name" validate:"required,min=3,max=20"`
+	Password     string             `json:"password" validate:"required,min=6"`
+	Email        string             `json:"email" validate:"required,email"`
+	UserType     string             `json:"user_type" validate:"required,oneof=ADMIN USER"`
+	RefreshToken string             `json:"refresh_token,omitempty"`
+	CreatedAt    time.Time          `json:"created_at"`
+	UpdatedAt    time.Time          `json:"updated_at"`
+}
