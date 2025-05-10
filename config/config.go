76f37247d69d6c04bcc5744098db9efe4bce9dbf
itsanindyak <iankoley04@gmail.com>
2025-05-11 01:33:15 +0530

    add configuration management with environment variable loading

---
+++ config/config.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	PORT                 string
+	MONGO_URI            string
+	TOKEN_KEY            []byte
+	TOKEN_EXPIRY         int
+	REFRESH_TOKEN_KEY    []byte
+	REFRESH_TOKEN_EXPIRY int
+)
+
+func init() {
+	var err error
+
+	
+	// Load .env only if not in production
+	if os.Getenv("ENV") != "production" {
+		err = godotenv.Load(".env")
+		if err != nil {
+			log.Println("⚠️ Could not load .env file, continuing with system environment variables")
+		}
+	}
+
+	// port
+
+	PORT = os.Getenv("PORT")
+
+	// Mongodb url
+
+	MONGO_URI = os.Getenv("MONGO_URI")
+
+	// Token key
+
+	TOKEN_KEY = []byte(os.Getenv("TOKEN_KEY"))
+
+	// Token Expire
+
+	TOKEN_EXPIRY, err = strconv.Atoi(os.Getenv("TOKEN_EXPIRY"))
+
+	if err != nil {
+		log.Fatal("❌ TOKEN_EXPIRY is not a valid integer")
+	}
+
+	// Token key
+
+	REFRESH_TOKEN_KEY = []byte(os.Getenv("REFRESH_TOKEN_KEY"))
+
+	// Token Expire
+
+	REFRESH_TOKEN_EXPIRY, err = strconv.Atoi(os.Getenv("REFRESH_TOKEN_EXPIRY"))
+
+	if err != nil {
+		log.Fatal("❌ REFRESH_TOKEN_EXPIRY is not a valid integer")
+	}
+
+}
