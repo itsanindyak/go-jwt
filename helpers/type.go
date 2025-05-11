38dcf1c6d5c9e6646eff430a8f241ce6f3ab6a58
itsanindyak <iankoley04@gmail.com>
2025-05-11 12:32:39 +0530

    fix: update Signup userType validation to include MODERATOR role

--- helpers/type.go
+++ helpers/type.go
@@ -9,6 +9,6 @@ type Signup struct {
 	FirstName string `json:"first_name" validate:"required,min=3,max=20"`
 	LastName  string `json:"last_name" validate:"required,min=3,max=20"`
 	Email     string `json:"email" validate:"required,email"`
-	UserType  string `json:"user_type" validate:"required,oneof=ADMIN USER"`
+	UserType  string `json:"user_type" validate:"required,oneof=ADMIN MODERATOR USER"`
 	Password  string `json:"password" validate:"required,min=6"`
 }
