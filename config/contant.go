a84d74db235ab378cded8dfca5ae9feebcfc059d
itsanindyak <iankoley04@gmail.com>
2025-05-11 12:32:45 +0530

    feat: add user permissions and roles constants

---
+++ config/contant.go
@@ -0,0 +1,13 @@
+package config
+
+// constants.go (or in a shared package like permissions)
+const (
+	PermissionReadUser   = "read_user"
+	PermissionDeleteUser = "delete_user"
+	PermissionUpdateUser = "update_user"
+	PermissionReadSelf   = "read_self"
+
+	RoleAdmin     = "ADMIN"
+	RoleModerator = "MODERATOR"
+	RoleUser      = "USER"
+)
