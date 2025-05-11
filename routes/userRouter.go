abe4990117c527744558d1844fcf61b21d804aec
itsanindyak <iankoley04@gmail.com>
2025-05-11 12:33:14 +0530

    refactor: reorganize UserRouter function for improved readability and maintainability

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -2,14 +2,27 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/itsanindyak/go-jwt/config"
 	"github.com/itsanindyak/go-jwt/controllers"
 	"github.com/itsanindyak/go-jwt/middleware"
 )
 
-func UserRouter(route *gin.RouterGroup){
+func UserRouter(route *gin.RouterGroup) {
 	route.Use(middleware.Authenticate())
-	route.GET("/",controllers.GetUsers())
-	route.GET("/:user_id",controllers.GetUser())
-	route.PATCH("/:user_id",controllers.UpdateUser())
-	route.DELETE("/:user_id",controllers.DeleteUser())
+
+	route.GET("/:user_id",
+		middleware.GrantMiddleware([]string{config.PermissionReadSelf, config.PermissionReadUser}),
+		controllers.GetUser())
+
+	route.GET("/",
+		middleware.GrantMiddleware([]string{config.PermissionReadUser}),
+		controllers.GetUsers())
+
+	route.PATCH("/:user_id",
+		middleware.GrantMiddleware([]string{config.PermissionUpdateUser}),
+		controllers.UpdateUser())
+
+	route.DELETE("/:user_id",
+		middleware.GrantMiddleware([]string{config.PermissionDeleteUser}),
+		controllers.DeleteUser())
 }
