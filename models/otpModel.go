2fc34fd3e4790bed3a16e7e2f31bdc120c1bcde5
itsanindyak <iankoley04@gmail.com>
2025-08-04 09:21:03 +0530

    feat: add OTP model for one-time password management

---
+++ models/otpModel.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type OTP struct {
+	ID     primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserID primitive.ObjectID `bson:"user_id" json:"user_id" validate:"required"`
+	Otp    string             `bson:"code" json:"otp" validate:"required,min=3,max=20"`
+	TTL    time.Time          `bson:"expireAt" json:"expire_at" validate:"required"` 
+	Used   bool               `bson:"used" json:"used"`
+}
