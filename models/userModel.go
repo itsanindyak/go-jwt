bf03b81c8feba54db159e8d5be75bc0bfacd523b
itsanindyak <iankoley04@gmail.com>
2025-08-04 09:21:19 +0530

    fix: update bson tags for User model fields for consistency

--- models/userModel.go
+++ models/userModel.go
@@ -8,12 +8,13 @@ import (
 
 type User struct {
 	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	FirstName    string             `json:"first_name" validate:"required,min=3,max=20"`
-	LastName     string             `json:"last_name" validate:"required,min=3,max=20"`
-	Password     string             `json:"password" validate:"required,min=6"`
-	Email        string             `json:"email" validate:"required,email"`
-	UserType     string             `json:"user_type" validate:"required,oneof=ADMIN MODERATOR USER"`
-	RefreshToken string             `json:"refresh_token,omitempty"`
-	CreatedAt    time.Time          `json:"created_at"`
-	UpdatedAt    time.Time          `json:"updated_at"`
+	FirstName    string             `bson:"first_name" json:"first_name" validate:"required,min=3,max=20"`
+	LastName     string             `bson:"last_name" json:"last_name" validate:"required,min=3,max=20"`
+	Password     string             `bson:"password" json:"password" validate:"required,min=6"`
+	Email        string             `bson:"email" json:"email" validate:"required,email"`
+	UserType     string             `bson:"user_type" json:"user_type" validate:"required,oneof=ADMIN MODERATOR USER"`
+	RefreshToken string             `bson:"refresh_token,omitempty" json:"refresh_token,omitempty"`
+	Verified     bool               `bson:"verified" json:"verified"`
+	CreatedAt    time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt    time.Time          `bson:"updated_at" json:"updated_at"`
 }
