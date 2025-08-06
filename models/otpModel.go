b125454c4bd0135fb90c8a8e5efe677986474069
itsanindyak <iankoley04@gmail.com>
2025-08-07 00:22:05 +0530

    fix: correct JSON field name for TTL in OTP model

--- models/otpModel.go
+++ models/otpModel.go
@@ -10,6 +10,6 @@ type OTP struct {
 	ID     primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	UserID primitive.ObjectID `bson:"user_id" json:"user_id" validate:"required"`
 	Otp    string             `bson:"code" json:"otp" validate:"required,min=3,max=20"`
-	TTL    time.Time          `bson:"expireAt" json:"expire_at" validate:"required"` 
+	TTL    time.Time          `bson:"expireAt" json:"expireAt" validate:"required"` 
 	Used   bool               `bson:"used" json:"used"`
 }
