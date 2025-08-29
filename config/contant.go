1bd776b19c05c2128cb60175cbac64dfc0619ee1
itsanindyak <iankoley04@gmail.com>
2025-08-29 09:24:52 +0530

    refactor: reorganize permission constants and role permissions mapping

--- config/contant.go
+++ config/contant.go
@@ -1,13 +1,30 @@
 package config
 
-// constants.go (or in a shared package like permissions)
+
 const (
-	PermissionReadUser   = "read_user"
-	PermissionDeleteUser = "delete_user"
-	PermissionUpdateUser = "update_user"
-	PermissionReadSelf   = "read_self"
+	PermissionReadUser       = "read_user"
+	PermissionDeleteUser     = "delete_user"
+	PermissionUpdateUser     = "update_user"
+	PermissionUpdateUserType = "update_user_type"
+	PermissionReadSelf       = "read_self"
 
 	RoleAdmin     = "ADMIN"
 	RoleModerator = "MODERATOR"
 	RoleUser      = "USER"
 )
+
+var RolePermissions = map[string]map[string]struct{}{
+	RoleAdmin: {
+		PermissionReadUser:       {},
+		PermissionDeleteUser:     {},
+		PermissionUpdateUser:     {},
+		PermissionUpdateUserType: {},
+	},
+	RoleModerator: {
+		PermissionReadUser:   {},
+		PermissionUpdateUser: {},
+	},
+	RoleUser: {
+		PermissionReadSelf: {},
+	},
+}
