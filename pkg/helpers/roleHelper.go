a5499ba23abeea1cd46652db2e708bd74326699e
itsanindyak <iankoley04@gmail.com>
2025-08-29 09:25:22 +0530

    refactor: replace local RolePermissions map with config.RolePermissions for consistency

--- pkg/helpers/roleHelper.go
+++ pkg/helpers/roleHelper.go
@@ -4,23 +4,8 @@ import (
 	"github.com/itsanindyak/go-jwt/config"
 )
 
-var RolePermissions = map[string]map[string]struct{}{
-	config.RoleAdmin: {
-		config.PermissionReadUser:   {},
-		config.PermissionDeleteUser: {},
-		config.PermissionUpdateUser: {},
-	},
-	config.RoleModerator: {
-		config.PermissionReadUser:   {},
-		config.PermissionUpdateUser: {},
-	},
-	config.RoleUser: {
-		config.PermissionReadSelf: {},
-	},
-}
-
 func HasPermissionByRole(role string, permission string) bool {
-	if perms, ok := RolePermissions[role]; ok {
+	if perms, ok := config.RolePermissions[role]; ok {
 		if _, exists := perms[permission]; exists {
 			return true
 		}
