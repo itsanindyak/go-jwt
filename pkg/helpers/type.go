02bfc66e8fa5f3c7f0ebbc763cfb5d98413c6175
itsanindyak <iankoley04@gmail.com>
2025-08-31 19:11:14 +0530

    feat: define messageType and enhance Response struct for improved API responses

--- pkg/helpers/type.go
+++ pkg/helpers/type.go
@@ -1,5 +1,19 @@
 package helpers
 
+type messageType string
+
+var (
+	Error   messageType = "error"
+	Success messageType = "success"
+)
+
+type Response struct {
+	StatusCode  int         `json:"status_code" validate:"required"`
+	MessageType messageType `json:"message_type" validate:"required"`
+	Message     string      `json:"message" validate:"required"`
+	Data        interface{} `json:"data,omitempty" `
+}
+
 type Login struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=6"`
@@ -13,11 +27,24 @@ type Signup struct {
 	Password  string `json:"password" validate:"required,min=6"`
 }
 
-type OTPReq struct{
+type OTPReq struct {
 	OTP string `json:"otp" validate:"required"`
 }
 
-type NameUpdate struct{
+type NameUpdate struct {
 	FirstName string `json:"first_name" validate:"required,min=3,max=20"`
 	LastName  string `json:"last_name" validate:"required,min=3,max=20"`
+}
+type EmailUpdate struct {
+	Email string `json:"email" validate:"required,email"`
+}
+type TypeUpdate struct {
+	UserType string `json:"user_type" validate:"required,oneof=ADMIN MODERATOR USER"`
+}
+
+type UpdateData struct {
+	FirstName string `json:"first_name,omitempty" validate:"omitempty,min=3,max=20"`
+	LastName  string `json:"last_name,omitempty" validate:"omitempty,min=3,max=20"`
+	Email     string `json:"email,omitempty" validate:"omitempty,email"`
+	UserType  string `json:"user_type,omitempty" validate:"omitempty,oneof=ADMIN MODERATOR USER"`
 }
